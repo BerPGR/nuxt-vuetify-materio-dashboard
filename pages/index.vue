a81faa214a431966888852bc0d8ff86b412f6930
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-03-20 16:45:27 -0300

    Added items list to optimize code

--- pages/index.vue
+++ pages/index.vue
@@ -42,16 +42,16 @@
                     mdi-dots-vertical
                   </v-icon>
                 </v-btn>
-                <v-col cols="12" class="pb-3">
+                <v-col cols="12" sm="3" class="pb-3" v-for="(item, i) in statistics" :key="i">
                   <div class="d-flex flex-row">
-                    <v-sheet rounded elevation="0" width="50" height="50" color="deep-purple lighten-1" class="ml-4 d-flex justify-center">
+                    <v-sheet rounded elevation="0" width="50" height="50" :color="item.color" class="ml-4 d-flex justify-center">
                       <v-icon large>
-                        mdi-arrow-top-right
+                        {{ item.icon }}
                       </v-icon>
                     </v-sheet>
                     <div class="d-flex flex-column ml-3">
-                      <p class="mb-0 body-2 grey--text">Sales</p>
-                      <h2 class="">245k</h2>
+                      <p class="mb-0 body-2 grey--text">{{ item.title }}</p>
+                      <h2 class="">{{ item.subtitle }}</h2>
                     </div>
                   </div>
                 </v-col>
@@ -69,6 +69,14 @@
 <script>
 export default {
   name: 'IndexPage',
+  data: () => ({
+    statistics: [
+      {color: 'deep-purple lighten-1', icon: 'mdi-arrow-top-right', title: 'Sales', subtitle: '245k'},
+      {color: 'light-green accent-3', icon: 'mdi-account-outline', title: 'Customers', subtitle: '12.5k'},
+      {color: 'amber lighten-1', icon: 'mdi-tag-outline', title: 'Product', subtitle: '1.54k'},
+      {color: 'cyan accent-2', icon: 'mdi-currency-usd', title: 'Revenue', subtitle: '$88k'},
+    ]
+  }),
 }
 </script>
 
