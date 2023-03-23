3e1ca978804561b7211b2b61a3e74531d6525cbc
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-03-23 15:13:26 -0300

    Added first nav list to drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,36 @@
     color="transparent"
       true
       fixed
+      floating
       app
     >
+      <v-toolbar color="transparent" elevation="0">
+        <v-img :src="require('../assets/help/logo.png')" contain height="20" width="0"></v-img>
+        <v-toolbar-title class="font-weight-bold">MATERIO</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-icon small>
+          mdi-circle-double
+        </v-icon>
+      </v-toolbar>
+
+      <v-list nav dense>
+        <v-list-item-group>
+          <v-list-item v-for="(item, i) in apps" :key="i" link>
+            <v-list-item-icon>
+              <v-icon>
+                {{ item.icon }}
+              </v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+        </v-list-item-group>
+
+      </v-list>
     </v-navigation-drawer>
+
     <v-app-bar color="transparent" flat app>
       <v-row>
         <v-col cols="12" sm="7">
@@ -48,7 +75,27 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
+      apps: [
+        { icon: 'mdi-calendar', title: "Calendar" },
+        { icon: 'mdi-message-text', title: "Chat" },
+        { icon: 'mdi-email', title: "Email" },
+        { icon: 'mdi-receipt', title: "Invoice" },
+        { icon: 'mdi-account', title: "User" },
+        { icon: 'mdi-file', title: "Pages" },
+      ]
     }
   },
 }
 </script>
+
+<style>
+.v-list-item--active {
+  background: rgb(77,8,156);
+background: linear-gradient(257deg, rgba(77,8,156,1) 0%, rgba(176,109,177,1) 100%);
+}
+
+.v-list--nav .v-list-item, .v-list--nav .v-list-item:before {
+  border-radius: 5px 40px 40px 5px;
+  background-color: transparent;
+}
+</style>
